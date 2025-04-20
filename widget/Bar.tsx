d9083c27603a1c4dc3ca8a8e3ef8bb7133034da4
AmadeusWM <63149896+AmadeusWM@users.noreply.github.com>
2025-04-20 23:54:36 +0200

    simple bar done

--- widget/Bar.tsx
+++ widget/Bar.tsx
@@ -39,12 +39,11 @@ function BatteryLevel() {
 
     return <box className="Battery"
       visible={bind(bat, "isPresent")}
-      spacing={3}
     >
         <icon icon={bind(bat, "batteryIconName")} />
-        <label label={bind(bat, "percentage").as(p =>
+        {/* <label label={bind(bat, "percentage").as(p =>
             `${Math.floor(p * 100)}%`
-        )} />
+        )} /> */}
     </box>
 }
 
@@ -76,6 +75,125 @@ function PowerProfileSelector() {
         </button>
 }
 
+type MmcliOutput = {
+    modem: {
+        "3gpp": {
+            "5gnr": {
+                "registration-settings": {
+                    "drx-cycle": string;
+                    "mico-mode": string;
+                };
+            };
+            "enabled-locks": string[];
+            "eps": {
+                "initial-bearer": {
+                    "dbus-path": string;
+                    "settings": {
+                        apn: string;
+                        "ip-type": string;
+                        password: string;
+                        user: string;
+                    };
+                };
+                "ue-mode-operation": string;
+            };
+            imei: string;
+            "operator-code": string;
+            "operator-name": string;
+            "packet-service-state": string;
+            pco: string;
+            "registration-state": string;
+        };
+        cdma: {
+            "activation-state": string;
+            "cdma1x-registration-state": string;
+            esn: string;
+            "evdo-registration-state": string;
+            meid: string;
+            nid: string;
+            sid: string;
+        };
+        "dbus-path": string;
+        generic: {
+            "access-technologies": string[];
+            bearers: string[];
+            "carrier-configuration": string;
+            "carrier-configuration-revision": string;
+            "current-bands": string[];
+            "current-capabilities": string[];
+            "current-modes": string;
+            device: string;
+            "device-identifier": string;
+            drivers: string[];
+            "equipment-identifier": string;
+            "hardware-revision": string;
+            manufacturer: string;
+            model: string;
+            "own-numbers": string[];
+            physdev: string;
+            plugin: string;
+            ports: string[];
+            "power-state": string;
+            "primary-port": string;
+            "primary-sim-slot": string;
+            revision: string;
+            "signal-quality": {
+                recent: string;
+                value: string;
+            };
+            sim: string;
+            "sim-slots": string[];
+            state: string;
+            "state-failed-reason": string;
+            "supported-bands": string[];
+            "supported-capabilities": string[];
+            "supported-ip-families": string[];
+            "supported-modes": string[];
+            "unlock-required": string;
+            "unlock-retries": string[];
+        };
+    };
+};
+
+function SignalStrength() {
+  const signal = Variable<MmcliOutput | null>(null).poll(5000, () => {
+      try {
+          const output = GLib.spawn_command_line_sync("mmcli -m 0 --output-json")[1]
+          if (!output) {
+            return null;
+          }
+          const outputString = new TextDecoder().decode(output);
+          const mmcliOutput: MmcliOutput = JSON.parse(outputString);
+          return mmcliOutput
+      } catch (e) {
+          console.error("Failed to fetch signal strength:", e);
+          return null;
+      }
+  });
+
+  return <box className="SignalStrength" visible={bind(signal).as(signal => signal?.modem?.generic?.state === "connected")}>
+      {bind(signal).as(signal => {
+          if (!signal || signal.modem.generic.state !== "connected") {
+              return null;
+          }
+          const signalValue = parseInt(signal.modem.generic["signal-quality"].value, 10);
+          let iconName = "network-cellular";
+          if (signalValue >= 75) {
+              iconName = "network-cellular-signal-excellent-symbolic";
+          } else if (signalValue >= 50) {
+              iconName = "network-cellular-signal-good-symbolic";
+          } else if (signalValue >= 25) {
+              iconName = "network-cellular-signal-ok-symbolic";
+          } else if (signalValue > 0) {
+              iconName = "network-cellular-signal-weak-symbolic";
+          } else {
+              iconName = "network-cellular-signal-none-symbolic";
+          }
+          return <icon icon={iconName} />
+      })}
+  </box>
+}
+
 export default function Bar(monitor: Gdk.Monitor) {
     const { TOP, LEFT, RIGHT } = Astal.WindowAnchor
 
@@ -92,16 +210,21 @@ export default function Bar(monitor: Gdk.Monitor) {
             <box hexpand >
               <Time />
             </box>
-            <box hexpand halign={Gtk.Align.END} >
+            <box
+              hexpand
+              halign={Gtk.Align.END}
+              // spacing={4}
+            >
               <PowerProfileSelector/>
               <button
                 className="Tools"
                 onClick={() => GLib.spawn_command_line_async("env XDG_CURRENT_DESKTOP=GNOME gnome-control-center wifi")}
               >
                 <box
-                  spacing={3}
+                  spacing={8}
                 >
                   <Wifi />
+                  <SignalStrength/>
                   <AudioSlider />
                   <BatteryLevel />
                 </box>
